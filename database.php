4492d6d21dc4465cf9407cac93a2c6a17b56fab8
RuslanYakushenko <ruslan.yakushenko.1998@gmail.com>
2021-08-15 11:26:49 +0300

    база данных

--- database.php
+++ database.php
@@ -6,10 +6,16 @@ function getAllFilms()
     try {
 
         //подключение к базе данных
-        $pdo = new PDO('mysql:host=percona_db;dbname=my_cv;charset=utf8', 'root', 'root');
+        $pdo = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root','');
         
         ///текст запроса
-        $sql = "SELECT * FROM films LIMIT 2";
+        // $sql = "SELECT * FROM films LIMIT 3";
+        $sql = "SELECT f.name, f.rating, f.description, f.image_url, GROUP_CONCAT(g.genre) AS genre_name
+        FROM films AS `f`
+        INNER JOIN films_ganres AS `fg` ON f.id = fg.film_id
+        INNER JOIN genres AS `g` ON g.id = fg.genre_id       
+        GROUP BY f.name, f.rating, f.description, f.image_url";
+        
     
         ///формируем объект запроса
         $statemant = $pdo->prepare($sql);
@@ -41,4 +47,38 @@ function getAllFilms()
     return [];
 }
 
+
+function getAllBooks()
+{
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root','');
+
+
+        $sql = "SELECT b.image, b.name, b.description, GROUP_CONCAT(g.genre) AS genre_name
+        FROM books AS `b`
+        INNER JOIN books_genres AS `bg` ON b.id = bg.book_id
+        INNER JOIN genres AS `g` ON g.id = bg.genre_id       
+        GROUP BY b.image, b.name, b.description";
+
+        $statemant = $pdo->prepare($sql);
+    
+        $statemant->execute();
+    
+        $statemant->setFetchMode(PDO::FETCH_ASSOC);
+    
+
+        $result = $statemant->fetchAll();
+
+        return $result;
+
+    
+    }   catch (Exception $ex) {
+            var_dump($ex->getMessage());
+        }
+    
+    return [];
+
+}
+
+?>
 
--- migrations/20_07_2021.sql
+++ migrations/20_07_2021.sql
@@ -7,15 +7,53 @@ CREATE TABLE `films` (
     PRIMARY KEY (`id`)
     ) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
 
-CREATE TABLE `genres` ( 
-    `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT ,
-     `genre` VARCHAR(128) NOT NULL ,
-      PRIMARY KEY (`id`)
-     ) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
 
-CREATE TABLE `film_ganres` (
-     `id` INT(11) NOT NULL AUTO_INCREMENT ,
-      `film_id` INT(11) NOT NULL ,
-      `genre_id` INT(11) NOT NULL ,
-       PRIMARY KEY (`id`), 
-       UNIQUE `uniq_film_id_genre_id` (`film_id`, `genre_id`)) ENGINE = InnoDB;
+CREATE TABLE `test`.`genres` ( 
+    `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT , 
+    `genre` VARCHAR(128) NOT NULL , 
+    PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE `test`.`films_ganres` ( 
+    `id` INT(11) NOT NULL AUTO_INCREMENT ,
+    `film_id` INT(11) NOT NULL , 
+    `genre_id` INT(11) NOT NULL , 
+    PRIMARY KEY (`id`),
+     UNIQUE `uniq_film_id_genre_id` (`film_id`, `genre_id`), 
+     UNIQUE `uniq_genre_id` (`genre_id`)
+    ) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
+
+
+CREATE TABLE `test`.`contact_form` ( 
+    `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT , 
+    `user_name` VARCHAR(128) NOT NULL , 
+    `user_email` VARCHAR(128) NOT NULL , 
+    `user_number` INT(22) UNSIGNED NOT NULL , 
+    PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE `test`.`message_form` ( 
+    `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT ,
+    `id_contact_form` INT(11) UNSIGNED NOT NULL ,
+    `message` TEXT NOT NULL ,
+    PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;
+
+
+
+CREATE TABLE `test`.`books` (
+    `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT , 
+    `image` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
+    `name` VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , 
+    `description` TEXT NOT NULL , 
+    PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB;
+
+CREATE TABLE `test`.`books_genres` ( 
+    `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT , 
+    `book_id` INT(11) UNSIGNED NOT NULL , 
+    `genre_id` INT(11) UNSIGNED NOT NULL , 
+    PRIMARY KEY (`id`), 
+    UNIQUE `uniq_book_id_genre_id` (`book_id`, `genre_id`)
+    ) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
+
